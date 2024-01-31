a0159d6692addc794aaf6f3a1017093c9b573e92
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-01-31 18:37:38 -0500

    Placeholder shows page.

---
+++ src/app/components/Shows.tsx
@@ -0,0 +1,50 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Merch = () => {
+    return (
+        <div id="shows" className="d-flex flex-column  align-items-center" style={{ height: "30vh" }}>
+            <h2 className="font-weight-bold mb-4 fs-1">Shows</h2>
+            <Container>
+                <Row className="my-2">
+                    <Col className="p-4">Nov 30 THU</Col>
+                    <Col className="p-4">Ulster Sports Club @ 7:30pm</Col>
+                    <Col className="p-4">Belfast, United Kingdom</Col>
+                </Row>
+                <Row className="my-2">
+                    <Col className="p-4">Nov 30 THU</Col>
+                    <Col className="p-4">Ulster Sports Club @ 7:30pm</Col>
+                    <Col className="p-4">Belfast, United Kingdom</Col>
+                </Row>
+                <Row className="my-2">
+                    <Col className="p-4">Nov 30 THU</Col>
+                    <Col className="p-4">Ulster Sports Club @ 7:30pm</Col>
+                    <Col className="p-4">Belfast, United Kingdom</Col>
+                </Row>
+                <Row className="my-2">
+                    <Col className="p-4">Nov 30 THU</Col>
+                    <Col className="p-4">Ulster Sports Club @ 7:30pm</Col>
+                    <Col className="p-4">Belfast, United Kingdom</Col>
+                </Row>
+                <Row className="my-2">
+                    <Col className="p-4">Nov 30 THU</Col>
+                    <Col className="p-4">Ulster Sports Club @ 7:30pm</Col>
+                    <Col className="p-4">Belfast, United Kingdom</Col>
+                </Row>
+                <Row className="my-2">
+                    <Col className="p-4">Nov 30 THU</Col>
+                    <Col className="p-4">Ulster Sports Club @ 7:30pm</Col>
+                    <Col className="p-4">Belfast, United Kingdom</Col>
+                </Row>
+                <Row className="my-2">
+                    <Col className="p-4">Nov 30 THU</Col>
+                    <Col className="p-4">Ulster Sports Club @ 7:30pm</Col>
+                    <Col className="p-4">Belfast, United Kingdom</Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Merch 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navigation from "./components/Navbar"
 import Landing from "./components/Landing"
 import Video from "./components/Video"
 import Merch from "./components/Merch"
+import Shows from "./components/Shows"
 
 export default function Home() {
     return (
@@ -12,6 +13,7 @@ export default function Home() {
             <Landing />
             <Video />
             <Merch />
+            <Shows />
         </main>
     );
 }

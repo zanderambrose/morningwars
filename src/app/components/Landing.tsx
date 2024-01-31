ebf22c6ff81443b96525d133e313cb38fbcdf14e
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-01-31 15:53:04 -0500

    First pass at Landing page.

---
+++ src/app/components/Landing.tsx
@@ -0,0 +1,22 @@
+const Landing = () => {
+    return (
+        <div id="home" className="d-flex align-items-center" style={{ height: "100vh" }}>
+            <div className="d-flex justify-content-evenly" style={{ width: "100%" }}>
+                <img
+                    src="/morningwarslogo.jpeg"
+                    width="500"
+                    height="500"
+                    className="d-inline-block align-top"
+                    alt="Album Cover"
+                />
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                    <h1>Prospect Gallery</h1>
+                    <p>Release Date</p>
+                    <p>Pre-Save Here</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import Navigation from "./components/Navbar"
+import Landing from "./components/Landing"
 
 export default function Home() {
     return (
         <main>
             <Navigation />
+            <Landing />
         </main>
     );
 }

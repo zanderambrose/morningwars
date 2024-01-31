92d7374ccf5e786f65461074cd0c23c98fc365cd
Zander Ambrose <alexander.d.ambrose@gmail.com>
2024-01-31 14:11:43 -0500

    Placeholder morning wars logo in nav.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,12 +5,20 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = () => {
     return (
-        <Navbar sticky="top" className="bg-body-tertiary">
+        <Navbar sticky="top" className="bg-body-tertiary" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="#home">Morning Wars Logo</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                        src="/morningwarslogo.jpeg"
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top"
+                        alt="Morning Wars logo"
+                    />
+                </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <FontAwesomeIcon icon={faBars} size="2xl" />
+                    <FontAwesomeIcon icon={faBars} size="2xl" color="white" />
                 </Navbar.Collapse>
             </Container>
         </Navbar>
